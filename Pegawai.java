7c1cc2f37391a0a6762a6e544d2ed552d0fb4e08
nnkens <nnkens@gmail.com>
2016-03-22 13:51:15 +0700

    Pegawai
    Pegawai

---
+++ Pegawai.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public  class  Pegawai  {
+	public  static  void  main(String...  args)  {
+		System.out.println("Masukkan Nomor Induk Pegawai:");
+		Scanner scan =  new  Scanner(System.in);
+		String nomorInduk  = scan.next();
+		String[] array = nomorInduk.split("\\.");
+		String departemen  = array[0];
+		
+		System.out.println("Masukkan Nama Lengkap:");
+		String namaPegawai  =  scan.next();
+		
+		System.out.println("Masukkan Usia:");
+		int usiaPegawai  =  scan.nextInt();
+		
+		System.out.println("Masukkan Jenis Kelamin:");
+		String jenisKelamin  =  scan.next();
+		
+		System.out.println("Nilai IPK Pegawai :");
+		float ipkPegawai  =  scan.nextFloat();;
+		
+		System.out.println("==============================");
+		System.out.println("Nomor Induk Pegawai :"+nomorInduk);
+		System.out.println("Departeman :"+departemen);
+		System.out.println("Nama Lengkap :"+namaPegawai);
+		System.out.println("Usia :"+usiaPegawai);
+		System.out.println("Jenis Kelamin :"+jenisKelamin);
+		System.out.println("IPK :"+ipkPegawai);
+		System.out.println("==============================");
+ 	}
+}
