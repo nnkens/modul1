3c1010f7de1ad9141c4e6c768499608378bea9ed
nnkens <nnkens@gmail.com>
2016-03-23 22:15:45 +0700

    Latihan 5.1

--- AppMahasiswa2.java
+++ AppMahasiswa2.java
@@ -10,9 +10,19 @@ public  class  AppMahasiswa2  {
 	public static	ArrayList<String>  noTelps  		=  new  ArrayList<>();
 	public static	ArrayList<String>  jenisKelamins  	=  new  ArrayList<>();
 	public static   Scanner scan 						=  new  Scanner(System.in);
+	public static	Scanner scanMenu 					=  new  Scanner(System.in);
+	public static 	byte	menu						=  0;
+	public static 	StringBuffer nomorInduk				=  new StringBuffer("");
+	public static	StringBuffer kodeProgStudi			=  new StringBuffer("");
+	public static	StringBuffer namaprogStudi			=  new StringBuffer("");
+	public static	StringBuffer nama					=  new StringBuffer("");
+	public static	StringBuffer alamatTinggal			=  new StringBuffer("");
+	public static	StringBuffer alamatAsal				=  new StringBuffer("");
+	public static	StringBuffer noTelp					=  new StringBuffer("");
+	public static	StringBuffer jenisKelamin			=  new StringBuffer("");
 	
 	public  static  void  main(String...  args)  {
-			showMenu();
+		showMenu();
  	}
 	
 	public static void showMenu()
@@ -23,68 +33,63 @@ public  class  AppMahasiswa2  {
 		System.out.println("=  2 : Cari Data Mahasiswa   =");
 		System.out.println("=  3 : EXIT                  =");
 		System.out.println("==============================");
-		Scanner scanMenu 	=  new  Scanner(System.in);
-		byte menu  			= scanMenu.nextByte();
-		if(menu==1){
-			entryMahasiswa();
-		}else if(menu==2){
-			cariMahasiswa();
-		}else{
-			keluarAplikasi();
+		menu  			= scanMenu.nextByte();
+		switch(menu)
+		{
+			case 1 : entryMahasiswa(); break;
+			case 2 : cariMahasiswa(); break;
+			case 3 : keluarAplikasi(); break;
 		}
 	}
 	
 	public static void entryMahasiswa()
 	{
-		String nomorInduk		= "";
-		String kodeProgStudi	= "";
-		String namaprogStudi 	= "";
-		
 		System.out.println(" ==== Entry Data Mahasiswa ==== ");
-		System.out.println("Masukkan NIM :");
-		nomorInduk 			= scan.nextLine();
-		String[] splitNim 			= nomorInduk.split("\\.");
+		System.out.println(" Masukkan NIM :");
+		nomorInduk.replace(0, nomorInduk.toString().length(), scan.nextLine());
+		String[] splitNim 			= nomorInduk.toString().split("\\.");
 		
 		try{
-			kodeProgStudi  	= splitNim[0];
+			kodeProgStudi.replace(0, kodeProgStudi.toString().length(), splitNim[0]);
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Format Tidak Sesuai (XXX.YYYY.XX)");
 			System.exit(0);
 		}
 		
 		
-		if (kodeProgStudi.equals("A11")) {
-			namaprogStudi = "Tekhnik Informatika (S1)";
-		}else if (kodeProgStudi.equals("A12")) {
-			namaprogStudi = "Sistem Informasi (S1)";
-		}else if (kodeProgStudi.equals("A13")) {
-			namaprogStudi = "Teknik Informatika (D3)";
-		}else if (kodeProgStudi.equals("A14")) {
-			namaprogStudi = "Sistem Informasi (D3)";
+		if (kodeProgStudi.toString().equals("A11")) {
+			namaprogStudi.replace(0, namaprogStudi.toString().length(), "Tekhnik Informatika (S1)");
+		}else if (kodeProgStudi.toString().equals("A12")) {
+			namaprogStudi.replace(0, namaprogStudi.toString().length(), "Sistem Informasi (S1)");
+		}else if (kodeProgStudi.toString().equals("A13")) {
+			namaprogStudi.replace(0, namaprogStudi.toString().length(), "Tekhnik Informatika (D3)");
+		}else if (kodeProgStudi.toString().equals("A14")) {
+			namaprogStudi.replace(0, namaprogStudi.toString().length(), "Sistem Informasi (D3)");
 		}
 		
 		System.out.println("Masukkan Nama Lengkap:");
-		String nama  =  scan.nextLine();
+		nama.replace(0, nama.toString().length(), scan.nextLine());
 		
 		System.out.println("Masukkan Alamat Tinggal (Jalan, Kota, Propinsi):");
-		String alamatTinggal  =  scan.nextLine();
+		alamatTinggal.replace(0, alamatTinggal.toString().length(), scan.nextLine());
 		
 		System.out.println("Masukkan Alamat Asal Mahasiswa (Jalan, Kota, Propinsi):");
-		String alamatAsal  =  scan.nextLine();
+		alamatAsal.replace(0, alamatAsal.toString().length(), scan.nextLine());
 		
 		System.out.println("No Telp / Hp :");
-		String noTelp  =  scan.nextLine();
+		noTelp.replace(0, noTelp.toString().length(), scan.nextLine());
 		
 		System.out.println("Jenis Kelamin (Pria/Wanita) :");
-		String jenisKelamin  =  scan.nextLine();;
+		jenisKelamin.replace(0, jenisKelamin.toString().length(), scan.nextLine());
 		
-		nomorInduks.add(nomorInduk);
-		namaprogStudis.add(namaprogStudi);
-		namas.add(nama);
-		alamatTinggals.add(alamatTinggal);
-		alamatAsals.add(alamatAsal);
-		noTelps.add(noTelp);
-		jenisKelamins.add(jenisKelamin);
+		nomorInduks.add(nomorInduk.toString());
+		namaprogStudis.add(namaprogStudi.toString());
+		namas.add(nama.toString());
+		alamatTinggals.add(alamatTinggal.toString());
+		alamatAsals.add(alamatAsal.toString());
+		noTelps.add(noTelp.toString());
+		jenisKelamins.add(jenisKelamin.toString());
+		
 		System.out.println(nomorInduk+" Berhasil disimpan...");
 		showMenu();
 	}
