be40a71ecff89710cac10a409f694353a49b23bf
nnkens <nnkens@gmail.com>
2016-03-23 22:47:30 +0700

    Latihan 6.1

--- ArrEmpatDimensi.java
+++ ArrEmpatDimensi.java
@@ -1,9 +1,24 @@
-//import java.util.Arrays;
+import java.util.Arrays;
 public  class  ArrEmpatDimensi  {
 	public  static  void  main(String...  args)  {
-		String[][][] karyawan = new String[3][2][3];
-		karyawan[0][0][0] = "T1.0001";    
-        karyawan[0][0][1] = "Nanang";   		
-        karyawan[0][0][2] = "Jakarta";        
+		int[][][] arr = new int[3][2][3];
+		arr[0][0][0] = 111;    
+        arr[0][0][1] = 112;   		
+        arr[0][0][2] = 113;
+		arr[0][1][0] = 121;    
+        arr[0][1][1] = 122;   		
+        arr[0][1][2] = 123; 
+		arr[1][0][0] = 211;    
+        arr[1][0][1] = 212;   		
+        arr[1][0][2] = 213;
+		arr[1][1][0] = 221;    
+        arr[1][1][1] = 222;   		
+        arr[1][1][2] = 223;
+		arr[2][0][0] = 311;    
+        arr[2][0][1] = 312;   		
+        arr[2][0][2] = 313;
+		arr[2][1][0] = 321;    
+        arr[2][1][1] = 322;   		
+        arr[2][1][2] = 323;	
 	}
 }
